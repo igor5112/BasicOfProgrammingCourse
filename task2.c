f9602cf770fd0e7e42dd3efa24da350ff012ccca
Igore4ek <igor02092005@gmail.com>
2024-04-21 01:49:04 +0300

    Arrange the rows of the matrix by the non-decreasing order of the largest row elements

--- task2.c
+++ task2.c
@@ -1,3 +1,31 @@
-//
-// Created by Admin on 21.04.2024.
-//
+#include "libs/data_structures/matrix/matrix.h"
+
+
+int getMax(int *a, int n) {
+    int max = a[0];
+    for (int i = 1; i < n; i++) {
+        if (a[i] > max) {
+            max = a[i];
+        }
+    }
+    return max;
+}
+
+
+void sortRowsByMaxElement(matrix m) {
+    for (int i = 0; i < m.nRows; i++) {
+        int max_index = i;
+        for (int j = i+1; j < m.nRows; j++) {
+            if (getMax(m.values[j], m.nCols) < getMax(m.values[max_index], m.nCols)) {
+                max_index = j;
+            }
+        }
+        if (max_index != i) {
+            swapRows(&m, i, max_index);
+        }
+    }
+}
+
+int main (){
+    return 0;
+}
