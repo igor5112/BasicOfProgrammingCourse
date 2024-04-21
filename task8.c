921b9fd105586915c9c622a817870eb516c27dbe
Igore4ek <igor02092005@gmail.com>
2024-04-21 15:27:30 +0300

    task8

--- task8.c
+++ task8.c
@@ -1,3 +1,36 @@
-//
-// Created by Admin on 21.04.2024.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "libs/data_structures/matrix/matrix.h"
+
+
+int getMinInArea(matrix m, int startRow, int startCol, int endRow, int endCol) {
+    int minVal = INT_MAX;
+    for (int i = startRow; i <= endRow; i++) {
+        for (int j = startCol; j <= endCol; j++) {
+            if (m.values[i][j] < minVal) {
+                minVal = m.values[i][j];
+            }
+        }
+    }
+    return minVal;
+}
+
+int main() {
+    matrix m;
+    m.nRows = 3;
+    m.nCols = 3;
+    m.values = (int **) malloc(m.nRows * sizeof(int *));
+    for (int i = 0; i < m.nRows; i++) {
+        m.values[i] = (int *) malloc(m.nCols * sizeof(int));
+    }
+
+    int minVal = getMinInArea(m, 0, 0, 2, 2);
+
+    printf("%d\n", minVal);
+    for (int i = 0; i < m.nRows; i++) {
+        free(m.values[i]);
+    }
+    free(m.values);
+    return 0;
+}
